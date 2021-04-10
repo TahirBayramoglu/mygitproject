46b5788edcfd7c56ae2b339bcdfdb096170e90d8
tahir <tahirbayramoglu@gmail.com>
2021-04-10 15:28:14 +0200

    son hali
    bu sefer son

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,23 +5,22 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("version 1");
 		System.out.println("My second commit");
-		
+
 		System.out.println("Bu github beni bitirdi");
-		
+
 		System.out.println("Yeni baslangiclar");
 		System.out.println("Uzak sunucudaki kodum");
-		
+
 		System.out.println("14. satir bilgisayarim");
 
 		System.out.println("14. satir uzak bilgisayar");
-		
 
-		
 		System.out.println("19. satir bilgisayarim");
-		
+
 		System.out.println("18. satir uzak sunucu");
+
+		System.out.println("24. satir uzak sunucu");
+
 	}
-	
-	
 
 }
